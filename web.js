cd0faeaf0252c38a05c11a426e42377dc70d754c
Rison Simon <risonsimon@gmail.com>
2013-07-09 15:23:05 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var k = fs.readFileSync('./index.html','utf8');
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(k);
 });
 
 var port = process.env.PORT || 5000;
